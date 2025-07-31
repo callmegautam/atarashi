061b1ad526f1f351787ac6ca6b349165909272c8
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-07-31 18:43:53 +0530

    react template

---
+++ templates/frontend-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Atarashi</h1>
+            <p>created by Gautam Suthar</p>
+        </div>
+    );
+};
+
+export default App;
